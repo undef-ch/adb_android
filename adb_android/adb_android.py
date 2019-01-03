5541ab6cd0873e9e68759ea487a64a343ca66574
Philip Whitfield <underdoeg@users.noreply.github.com>
2019-01-03 22:58:06 +0100

    python3 compatibility fix

--- adb_android/adb_android.py
+++ adb_android/adb_android.py
@@ -197,7 +197,7 @@ def _exec_command(adb_cmd):
         result = e.returncode, t.read()
     else:
         result = 0, output
-        print('\n' + result[1])
+        print('\n' + result[1].decode("utf-8"))
 
     return result
 
